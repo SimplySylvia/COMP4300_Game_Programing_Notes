cd7ab3ef5207ef124afa276583c190290ccc3d77
DaltonHart <daltonhart.j@gmail.com>
2023-02-22 14:51:09 -0800

    adds base of code for SFML within vscode

--- lecture04_sfml/drawn_shapes.cpp
+++ lecture04_sfml/drawn_shapes.cpp
@@ -8,4 +8,35 @@ int main(int argc, char *argv[])
   const int wWidth = 400;
   const int wHeight = 400;
   sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "SFML works!");
+  window.setFramerateLimit(60);
+
+  // create a shape
+  int r = 0;
+  int g = 255;
+  int b = 255;
+
+  sf::CircleShape shape(50);
+  shape.setFillColor(sf::Color(r, g, b));
+  shape.setPosition(200.0f, 200.0f);
+  float shapeMoveSpeed = 0.01f;
+
+  // While windows is open. Draw shape.
+  while (window.isOpen())
+  {
+    sf::Event event;
+
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+      {
+        window.close();
+      }
+
+      window.clear();
+      window.draw(shape);
+      window.display();
+    }
+  }
+
+  return 0;
 }
